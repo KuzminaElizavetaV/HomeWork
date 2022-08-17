5c3316bf7acc2d5722e3c92e21a2ba21b99a8908
Elizaveta <hamlizuha@mail.ru>
2022-08-17 12:52:26 +0300

    семинар 2

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое число: ");
+int  NumberB = int.Parse(Console.ReadLine());
+int Num1 = NumberB % 1000;
+if (Num1 > 99 && Num1 < 1000) {
+    int Num2 = Num1 / 100;
+    Console.WriteLine(Num2);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
